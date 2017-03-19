d2aea9181694d7373855d70e780caf75ba003fb6
lijiarui <ruiruibupt@126.com>
2017-03-19 14:25:54 +0800

    changeFeeLink

--- robot/config/config.ts
+++ robot/config/config.ts
@@ -3,11 +3,11 @@ const Config = {
     TULING123_API_KEY: '17f03a4eff2c4bd095ff480b57150e8f',
 
     // 收费功能
-    PAY_LINK: "http://weixin.ibanling.com/jdzfpaydev/items/12308?postscript_seller=",
+    PAY_LINK: "http://weixin.ibanling.com/jdzfpay/items/12308?postscript_seller=",
     FEE_ROOM_NAME: "test付费群",
 
     // 收费功能
-    // PAY_LINK: "http://weixin.ibanling.com/jdzfpaydev/items/12163?postscript_seller=",
+    // PAY_LINK: "http://weixin.ibanling.com/jdzfpay/items/12163?postscript_seller=",
     // FEE_ROOM_NAME: "天使汇VIP社群",
 
     SCKEY: "SCU3520T4b3e8b858e984f3ab0b0f609ad04abce583e3f42c4079",
